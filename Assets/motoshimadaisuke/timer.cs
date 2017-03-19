64352f0a0380fb8b3cfc8e2c87ec871477ef1370
motoshimadaisuke <doragon111999111999@yahoo.co.jp>
2017-03-19 23:49:42 +0900

    timer 時間が減るスクリプト 時間が０になるとGameEndがtrueになる

---
+++ Assets/motoshimadaisuke/timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour {
+    public Gamemanager obj_manager;
+    [SerializeField]
+    private float time = 60;
+
+
+    void Start()
+    {
+        GetComponent<Text>().text = ((int)time).ToString();
+    }
+
+    void Update()
+    {
+        //1秒に1ずつ減らしていく
+        time -= Time.deltaTime;
+        //マイナスは表示しない
+        if (time < 0)
+        {
+            time = 0;
+            obj_manager.time_end = 1;
+
+            
+        }
+        GetComponent<Text>().text = ((int)time).ToString();
+    }
+}
