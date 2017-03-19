9a62175488e51e48cdfd40665b8a925c884bdf1d
naonosuke8267 <naonosuke8267@gmail.com>
2017-03-19 16:29:44 +0900

    もこみちの左右移動(滑る)実装 ↓キーで物をつかむ作成中

---
+++ Assets/Kyano/ProtoType/Scripts/Mokomichi.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mokomichi : MonoBehaviour {
+
+	private float SPD_MAX = 2;
+
+	public float spd_player;
+	
+	int []unk = {0,1,2};
+
+	public enum MokomichiState{
+		stay,		//画面外での生成待ち
+		fadeIn,		//来客
+		active,		//入力待ち
+		fadeOut,	//怒って帰る
+		catching	//バンズでキャッチ！
+	}
+
+	public MokomichiState enu_state;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Rigidbody2D> ().AddForce (new Vector2(50,100));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		switch(enu_state){
+		case MokomichiState.active:
+			//初期化処理
+			float baf_spdX = 0;
+
+			//移動処理:左右キーで移動(滑る)
+			if (Input.GetButton ("Horizontal")) {
+				if (Input.GetAxis ("Horizontal") > 0) {
+					baf_spdX = spd_player;
+				} else {
+					baf_spdX = -spd_player;
+				}
+			}
+
+			//一定速度でリミッターをかける
+			if (Mathf.Abs (GetComponent<Rigidbody2D> ().velocity.x) < SPD_MAX) {
+				GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x + baf_spdX, GetComponent<Rigidbody2D> ().velocity.y);
+			}
+
+			//下キーでバンズをキャッチ、ステートを移行
+			if (Input.GetButton ("Vertical") && Input.GetAxis("Vertical") < 0) {
+				enu_state = MokomichiState.catching;
+			}
+
+			break;
+
+		case MokomichiState.catching:
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0,0);
+
+
+			break;
+
+		default:
+			break;
+		}
+
+	}
+}
