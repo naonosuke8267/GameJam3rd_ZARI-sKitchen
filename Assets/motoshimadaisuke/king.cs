8574aecfb4cfe29712520b1bdb43a0325320ed16
motoshimadaisuke <doragon111999111999@yahoo.co.jp>
2017-03-19 18:47:32 +0900

    ザリガニの当たり判定とプレイヤーからのデータの受け取りのスクリプトを含んだプレハブを作成

---
+++ Assets/motoshimadaisuke/king.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class king : MonoBehaviour {
+    public int vector_x;
+    public int vector_y;
+    // Use this for initialization
+    void Start () {
+        set();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void set()
+    {
+        GetComponent< Rigidbody >(). AddForce(new Vector2(vector_x, vector_y));
+    }
+}
