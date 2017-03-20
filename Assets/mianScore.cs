21b2a24042d94fced1dba9ce7640c4d54c1c2906
motoshimadaisuke <mmmm0909111@gmail.com>
2017-03-20 15:17:12 +0900

    リザルト実装

---
+++ Assets/mianScore.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class mianScore : MonoBehaviour {
+    public int many;
+	// Use this for initialization
+	void Start () {
+        many = Mokomichi.num_catchZariganis[3];//ここを変える
+        GetComponent<Text>().text = ((int)many).ToString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/motoshimadaisuke/Score_2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Score_2 : MonoBehaviour {
+    public int N;
+	// Use this for initialization
+	void Start () {
+        N = Mokomichi.num_catchZariganis[1];
+        GetComponent<Text>().text = ((int)N).ToString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
