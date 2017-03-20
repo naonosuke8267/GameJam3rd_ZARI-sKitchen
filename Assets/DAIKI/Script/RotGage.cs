eae432efb8c063ea2439896979325cee9c68deae
倉田大暉 <denson.drum@gmail.com>
2017-03-20 13:32:51 +0900

    角度メーター設置

---
+++ Assets/DAIKI/Script/RotGage.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotGage : MonoBehaviour
+{
+
+    float z = 0f;
+    int flg = 0;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        transform.rotation = Quaternion.Euler (0,0,z);
+        //y -= 0.1f;
+        if (flg == 1)
+        {
+            z += 5f;
+            if (z >= 0)
+            {
+                flg = 0;
+            }
+        }
+        else if (flg == 0)
+        {
+            z -= 5f;
+            if (z <= -90)
+            {
+                flg = 1;
+            }
+        }
+    }
+
+}
